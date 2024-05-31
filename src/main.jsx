7b98f5fa901cf4cee633f87b98b7e025673219fc
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-06-01 02:24:36 +0600

    Auth context added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
+import AuthProvider from "./Contexts/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
+      <AuthProvider>
       <RouterProvider router={router} />
+      </AuthProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
