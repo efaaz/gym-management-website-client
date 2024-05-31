ffd03af574281733c3195b83c6e469ee31ed7da5
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-06-01 02:26:11 +0600

    404 error page added

--- src/ErrorPage/Error.jsx
+++ src/ErrorPage/Error.jsx
@@ -7,9 +7,9 @@ function Error() {
       <div className="flex justify-center items-center h-screen  gap-2">
         <div>
           <h1 className="text-7xl text-red-600 mb-4">
-            404 <span className="text-black">error</span>
+            404 <span className="text-white">error</span>
           </h1>
-          <p className="text-7xl">Not Found</p>
+          <p className="text-7xl text-white">Not Found</p>
           <div className=" text-center mt-4">
             <Link to="/" className="underline font-medium">
               Go back to Home
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Components/Home/Home/Home";
 import Layout from "../Layout/Layout";
+import Error from "../ErrorPage/Error";
 
 export const router = createBrowserRouter([
   {
