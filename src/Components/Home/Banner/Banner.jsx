e35f2a669305d9417e71664af4f448dad30740f3
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-06-01 02:25:46 +0600

    banner added

---
+++ src/Components/Home/Banner/Banner.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import banner1 from "../../../assets/Banner/banner1.jpg";
+import banner2 from "../../../assets/Banner/banner2.jpg";
+import banner3 from "../../../assets/Banner/banner3.jpg";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { Autoplay, EffectFade } from "swiper/modules";
+import { Link } from "react-router-dom";
+import "./style.css"; // Import the CSS file
+
+function Banner() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        autoplay={{
+          delay: 3100,
+          disableOnInteraction: false,
+        }}
+        effect={"fade"}
+        modules={[Autoplay, EffectFade]}
+        className="mySwiper "
+      >
+        <SwiperSlide>
+          <div
+            className="hero custom-hero-height"
+            style={{
+              backgroundImage: `url(${banner1})`,
+            }}
+          >
+            <div className="hero-overlay custom-hero-overlay bg-[#080303] bg-opacity-65"></div>
+            <div className="hero-content text-neutral-content">
+              <div className="max-w-max">
+                <h1 className="md:text-5xl text-2xl pb-2 text-white font-semibold">
+                  Stay fit and healthy{" "}
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  with our custom
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  fitness tracker platform
+                </h1>
+                <p className="my-5 text-sm md:text-base font-normal pr-64 text-white">
+                  Our experience trainers will wrok with you to create a
+                  personalized workout classes that fits your schedule fitness
+                  level, and objectives.
+                </p>
+                <Link to="/services" className="btn bg-[#771333] text-white">
+                  Explore Classes
+                </Link>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="hero custom-hero-height"
+            style={{
+              backgroundImage: `url(${banner2})`,
+            }}
+          >
+            <div className="hero-overlay custom-hero-overlay bg-[#080303] bg-opacity-65"></div>
+            <div className="hero-content text-neutral-content">
+              <div className="max-w-max">
+                <h1 className="md:text-5xl text-2xl pb-2 text-white font-semibold">
+                  Stay fit and healthy{" "}
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  with our custom
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  fitness tracker platform
+                </h1>
+                <p className="my-5 text-sm md:text-base font-normal pr-64 text-white">
+                  Our experience trainers will wrok with you to create a personalized workout classes that fits your schedule fitness level, and objectives.
+                </p>
+                <Link to="/services" className="btn bg-[#771333] text-white">
+                  Explore Classes
+                </Link>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="hero custom-hero-height"
+            style={{
+              backgroundImage: `url(${banner3})`,
+            }}
+          >
+            <div className="hero-overlay custom-hero-overlay bg-[#080303] bg-opacity-65"></div>
+            <div className="hero-content text-neutral-content">
+              <div className="max-w-max">
+                <h1 className="md:text-5xl text-2xl pb-2 text-white font-semibold">
+                  Stay fit and healthy{" "}
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  with our custom
+                </h1>{" "}
+                <h1 className="md:text-5xl pb-2 text-2xl text-white font-semibold">
+                  fitness tracker platform
+                </h1>
+                <p className="my-5 text-sm md:text-base font-normal pr-64 text-white">
+                  Our experience trainers will wrok with you to create a
+                  personalized workout classes that fits your schedule fitness
+                  level, and objectives.
+                </p>
+                <Link to="/services" className="btn bg-[#771333] text-white">
+                  Explore Classes
+                </Link>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
+
+export default Banner;
