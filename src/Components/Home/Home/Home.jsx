cbea6bf70b7f0eded39db5ad0f7f3c2171fd1193
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-06-02 02:54:04 +0600

    added images

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
 import Banner from '../Banner/Banner'
+import FeatureSection from '../FeaturedSection/FeatureSection'
+import AboutSection from '../AboutSection/AboutSection'
+import FeaturedClasses from '../FeaturedClasses/FeaturedClasses'
 
 function Home() {
   return (
@@ -9,7 +12,9 @@ function Home() {
         <title>Aura Fitness | Home</title>
     </Helmet>
     <Banner></Banner>
-    
+    <FeatureSection></FeatureSection>
+    <AboutSection></AboutSection>
+    <FeaturedClasses></FeaturedClasses>
     
     </>
   )
