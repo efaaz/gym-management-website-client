e60822391e037c9d72736554686a177bf08ae0fe
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-06-08 18:41:25 +0600

    admin page newsletter subscribers

--- src/Components/Dashboard/Admin/NewsLetterSubscribers/Subscribers.jsx
+++ src/Components/Dashboard/Admin/NewsLetterSubscribers/Subscribers.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import Spinner from "../../../Common/Loading/Spinner";
+import avatar from "../../../../assets/Avatar/avatar.png"
+import { useQuery } from "@tanstack/react-query";
 
 function Subscribers() {
+  const axiosPublic = useAxiosPublic();
   const fetchSubcribers = async (page) => {
-    const response = await axios.get(
-      `https://gym-management-livid.vercel.app/subscribers`
-    ); // Replace with your API endpoint
+    const response = await axiosPublic.get(`/subscribers`); // Replace with your API endpoint
     return response.data;
   };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["subscribers"],
+    queryFn: fetchSubcribers,
+  });
+
+  if (isLoading) return <Spinner></Spinner>;
+  if (error) return <div>Error loading trainers</div>;
   return (
     <>
       <Helmet>
         <title>ServiceSphere | To Do</title>
       </Helmet>
+      <h1 className="text-center md:text-3xl text-xl">Newsletter Subscribers Information</h1>
       <div className="overflow-x-auto max-w-7xl mx-auto">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
-              {/* <th>Service Name</th> */}
+              <th></th>
               <th>Client info</th>
               {/* <th>Service taking Date</th> */}
               <th>Status</th>
@@ -26,34 +38,29 @@ function Subscribers() {
           </thead>
           <tbody>
             {/* row 1 */}
-            {data.map((service) => (
-              <tr key={service._id}>
+            {data.map((user,idx) => (
+              <tr key={user._id}>
+                <td>{idx+1}</td>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
                       <div className="mask mask-squircle w-12 h-12">
                         <img
-                          src={service.userImg}
+                          src={avatar}
                           alt="Avatar Tailwind CSS Component"
                         />
                       </div>
                     </div>
                     <div>
-                      <div className="font-bold">{service.userName}</div>
+                      <div className="font-bold">{user.name}</div>
                       <div className="text-sm opacity-50">
-                        {service.userEmail}
+                        {user.email}
                       </div>
                     </div>
                   </div>
                 </td>
                 <th>
-                  <select className="select select-ghost max-w-xs">
-                    <option value="Pending" defaultChecked>
-                      Pending
-                    </option>
-                    <option value="Working">Working</option>
-                    <option value="Completed">Completed</option>
-                  </select>
+                 Subscribed
                 </th>
               </tr>
             ))}
